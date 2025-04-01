cad8f320296e9c2d7ed7f50bcd2401c06ec24d1a
alenorgue <alenoriegag@gmail.com>
2025-04-01 11:54:18 +0200

    Dom finished

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,13 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+const btnSend = document.getElementById("btn-send");
+const inputMessage = document.getElementById("message");
+const lastMessage = document.getElementById("last-message");
+//Definimos las variables que vamos a usar en la funcion y como EventListener, la funcion es simple, pero necesitamos .toUpperCase para las mayusculas.
+
+function getMessage() {
+lastMessage.textContent = inputMessage.value.toUpperCase();
+}
+
+btnSend.addEventListener('click', getMessage)
