7fc111f6e8dc96ff2dbec5d441903c0e9a1a5c38
alenorgue <alenoriegag@gmail.com>
2025-04-08 14:45:28 +0200

    finished APIs

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,18 @@
 async function getPuzzle(wordCount) {
-    let hiddenSentence = "Buena Suerte";
 
-    return hiddenSentence;
+    try {
+        const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`);
+
+        const data = await response.json();
+        return data.puzzle;
+
+    }
+
+    catch (error) {
+        console.error("Error fetching puzzle:", error);
+        return "No puzzle could be fetched.";
+    }
+
 }
+
+
